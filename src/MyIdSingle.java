27b60914d1f38d793e40faae50bfeb244444424c
arafinalif63 <mmdsarafatharafin@gmail.com>
2023-05-26 16:46:09 +0600

    address changed

--- src/MyIdSingle.java
+++ src/MyIdSingle.java
@@ -1,5 +1,5 @@
 public class MyIdSingle {
     public static void main(String[] args){
-        System.out.print("Arafin Alif \n0176666221 \nBarishal ");
+        System.out.print("Arafin Alif \n0176666221 \nBarishal Sadar, Barishal ");
     }
 }
