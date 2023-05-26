8dbc96cfeb480f090a90f07a51d05a5f67ad918c
arafinalif63 <mmdsarafatharafin@gmail.com>
2023-05-26 16:43:26 +0600

    print and println

---
+++ src/MyId.java
@@ -0,0 +1,7 @@
+public class MyId{
+    public static void main(String[] args){
+        System.out.println("Arafin Alif");
+        System.out.println("0176666221");
+        System.out.println("Barishal");
+    }
+}
