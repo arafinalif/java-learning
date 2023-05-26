2667eebb4518a638aee0f6b1a00a3bf8a8bd9f38
arafinalif63 <mmdsarafatharafin@gmail.com>
2023-05-26 16:53:24 +0600

    Sum of two number

---
+++ src/Sumoftwonum.java
@@ -0,0 +1,8 @@
+public class Sumoftwonum {
+    public static void main(String[] args) {
+        int x;
+        x = 20 + 20;
+        System.out.println("Sum of two num = " +x);
+        
+    }
+}
