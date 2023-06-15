557a975a1f5c23fdef41d264f4d349197f1ffc88
arafinalif63 <mmdsarafatharafin@gmail.com>
2023-06-15 21:02:21 +0600

    code update

--- src/FindThreeNumber.java
+++ src/FindThreeNumber.java
@@ -1,19 +1,22 @@
+import java.util.Scanner;
+
 public class FindThreeNumber {
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 20;
-        int num3 = 15;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1st number: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter 2nd number: ");
+        int num2 = scanner.nextInt();
+        System.out.println("Enter 3rd number: ");
+        int num3 = scanner.nextInt();
         int biggest;
-
-        if (num1 >= num2 && num1 >= num3) {
+        if (num1 > num2 &&  num1 > num3) {
             biggest = num1;
-        } else if (num2 >= num1 && num2 >= num3) {
+        } else if (num2 > num1 && num2 > num3) {
             biggest = num2;
         } else {
             biggest = num3;
         }
-
         System.out.println("The biggest number is: " + biggest);
-
     }
 }
